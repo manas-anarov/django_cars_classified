b921b67882568c5bafebadc74bc098de734ae137
manas-anarov <microsoft.kg@mail.ru>
2019-03-06 22:48:03 +0600

    Create Model Cars

---
+++ cars/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add = True)
+	brand = models.IntegerField(default=0)
+	title = models.CharField(max_length=400, default=0)
+	text = models.CharField(max_length=400, default=0)
+	price = models.IntegerField(default=0)
+	def __str__(self):
+		return self.title
