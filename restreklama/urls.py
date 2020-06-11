bcc78f07449a78cda85cb57cb87fd0faae6554e5
Your Name <you@example.com>
2020-06-11 21:26:03 +0600

    Delete trash, url restreklama

--- restreklama/urls.py
+++ restreklama/urls.py
@@ -5,17 +5,8 @@ from django.urls import path
 urlpatterns = [
 	path('add/car/', views.AddPostCar.as_view(), name='add-car'),
 	path('add/item/', views.AddPostItem.as_view(), name='add-item'),
-
-
-	# path('create/<id>/', views.CreateItemApiView.as_view(), name='item-create'),
-	# url(r'^add/car/$', views.AddPostCar.as_view(), name='add-car'),
-	# url(r'^add/item/$', views.AddPostItem.as_view(), name='add-item'),
-	# url(r'^create/(?P<id>[\w-]+)/$', views.CreateItemApiView.as_view(), name='item-create'),
-
-
-	url(r'^list/$', views.ListAPIView.as_view(), name='restcar-list'),
-	url(r'^detail/(?P<id>[\w-]+)/$', views.DetailApiView.as_view(), name='detail'),
-	url(r'^profile/list/$', views.ProfileListAPIView.as_view(), name='restcar-profile-list'),
-	url(r'^edit/(?P<id>[\w-]+)/$', views.EditPost.as_view(), name='restcar-edit'),
-
+	path('list/', views.ListAPIView.as_view(), name='list'),
+	path('detail/<id>/', views.DetailApiView.as_view(), name='detail'),
+	path('profile/list/', views.ProfileListAPIView.as_view(), name='profile-list'),
+	path('edit/<id>/', views.ProfileListAPIView.as_view(), name='edit'),
 ]
